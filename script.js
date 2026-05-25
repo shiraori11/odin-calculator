9972c80e9c405e36920d540abb53cfd302d72c3f
shiraori11 <shiraori@proton.me>
2026-05-25 19:08:30 +0800

    Reworked calculator logic I changed how the calculator works now it's what expected by TOP(final project on foundations) based on the bugs that needed to be fix.
    This implementation is a bruteforce way of making the buttons behave as per requirements. This might be the slowest way to do things but I think it's kind of beautiful since I made this, I did this logic. It might be the slowest and the worst way to solve this but, it's mine

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
-const calculate = function(calcu) {
-  const splitCalcu = calcu.split(/[+-/*]/)
-  const x = parseInt(splitCalcu[0]);
-  const y = parseInt(splitCalcu[1]);
-  
-  if (calcu.indexOf('+') > -1) {
-    return x + y;
-  } else if (calcu.indexOf('-') > -1) {
-    return x - y;
-  } else if (calcu.indexOf('*') > -1) {
-    return x * y;
-  } else if (calcu.indexOf('/') > -1) {
-    return x / y;
+const calculate = function(num1, num2, operator) {
+  // const splitCalcu = calcu.split(/[+-/*]/)
+  // const x = parseInt(splitCalcu[0]);
+  // const y = parseInt(splitCalcu[1]);
+  //
+  // if (calcu.indexOf('+') > -1) {
+  //   return x + y;
+  // } else if (calcu.indexOf('-') > -1) {
+  //   return x - y;
+  // } else if (calcu.indexOf('*') > -1) {
+  //   return x * y;
+  // } else if (calcu.indexOf('/') > -1) {
+  //   return x / y;
+  // }
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "/":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    
   }
 }
 
 const changeDisplay = function(button) {
-  
-  if (displayCalculation.textContent === "Calculator" || displayCalculation.textContent === "0") {
+  // if (displayCalculation.textContent === "Calculator" || displayCalculation.textContent === "0") {
+  //   displayCalculation.textContent = button;
+  // } else if (button === "=") {
+  //   displayCalculation.textContent = calculate(displayCalculation.textContent);
+  // } else if (displayCalculation.textContent.search(/[+-/*]/) > -1 && button.search(/[+-/*]/) > -1) {
+  //   displayCalculation.textContent = calculate(displayCalculation.textContent);
+  // } else if (button === "C") {
+  //   displayCalculation.textContent = 0;
+  // } else {
+  //   displayCalculation.textContent += button;
+  // }
+  currentDisplay = displayCalculation.textContent;
+
+  if (currentDisplay === "0") {
     displayCalculation.textContent = button;
-  } else if (button === "=") {
-    displayCalculation.textContent = calculate(displayCalculation.textContent);
-  } else if (displayCalculation.textContent.search(/[+-/*]/) > -1 && button.search(/[+-/*]/) > -1) {
-    displayCalculation.textContent = calculate(displayCalculation.textContent);
-  } else if (button === "C") {
-    displayCalculation.textContent = 0;
+  } else if (button.search(/[+-/*]/) > -1) {
+    if (calculatorOperator !== undefined) {
+      calculatorNumTwo = displayCalculation.textContent;
+      
+      calculatedNum = calculate(calculatorNumOne, calculatorNumTwo, calculatorOperator);
+      
+      displayCalculation.textContent = calculatedNum;
+      calculatorNumOne = calculatedNum;
+      calculatorNumTwo = "calculated";
+      // calculatorOperator = undefined;
+    } else {
+      calculatorNumOne = displayCalculation.textContent;
+      calculatorOperator = button;
+      displayCalculation.textContent = 0;
+    }
   } else {
-    displayCalculation.textContent += button;
+    if (calculatorOperator !== undefined && calculatorNumTwo === "calculated") {
+      displayCalculation.textContent = button;
+      calculatorNumTwo = 0
+    } else {
+      displayCalculation.textContent += button;
+    }
   }
 }
 
 const displayCalculation = document.querySelector(".display");
 const calculatorButtons = document.querySelectorAll("button");
+let calculatorNumOne;
+let calculatorNumTwo;
+let calculatorOperator;
 
 calculatorButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -38,5 +80,3 @@ calculatorButtons.forEach((button) => {
     changeDisplay(buttonValue);
   });
 });
-
-console.log(calculate("1182*123"));
