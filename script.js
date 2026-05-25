07a4a237e0eea8ebebe567f047d6afaffc7b86f7
shiraori11 <shiraori@proton.me>
2026-05-25 18:13:24 +0800

    Added design and more function to calc I changed added style to calculator. I added more function to the calculator to satisfy some of the requirements in the project requirements.
    I now realized that maybe my setup is actually on calculation is kind of rough and is not expected by the TOP? I am reading some of the bug I should look for and my current calculator is not what some of the bug fix is for?

--- script.js
+++ script.js
@@ -16,10 +16,14 @@ const calculate = function(calcu) {
 
 const changeDisplay = function(button) {
   
-  if (displayCalculation.textContent === "Calculator") {
+  if (displayCalculation.textContent === "Calculator" || displayCalculation.textContent === "0") {
     displayCalculation.textContent = button;
   } else if (button === "=") {
     displayCalculation.textContent = calculate(displayCalculation.textContent);
+  } else if (displayCalculation.textContent.search(/[+-/*]/) > -1 && button.search(/[+-/*]/) > -1) {
+    displayCalculation.textContent = calculate(displayCalculation.textContent);
+  } else if (button === "C") {
+    displayCalculation.textContent = 0;
   } else {
     displayCalculation.textContent += button;
   }
