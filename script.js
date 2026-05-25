4c54b2aa7cba1e3f05453e40ccada385e22c9aa4
shiraori11 <shiraori@proton.me>
2026-05-25 11:36:40 +0800

    Added button functions I connected the JS functions to the frontend. You can now click number button use operator and calculate the calculation as long as it follows the: operand operator operand

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let test = "1+1"
-
 const calculate = function(calcu) {
   const splitCalcu = calcu.split(/[+-/*]/)
   const x = parseInt(splitCalcu[0]);
@@ -15,5 +13,26 @@ const calculate = function(calcu) {
     return x / y;
   }
 }
+
+const changeDisplay = function(button) {
+  
+  if (displayCalculation.textContent === "Calculator") {
+    displayCalculation.textContent = button;
+  } else if (button === "=") {
+    displayCalculation.textContent = calculate(displayCalculation.textContent);
+  } else {
+    displayCalculation.textContent += button;
+  }
+}
+
+const displayCalculation = document.querySelector(".display");
+const calculatorButtons = document.querySelectorAll("button");
+
+calculatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttonValue = button.textContent;
+    changeDisplay(buttonValue);
+  });
+});
 
 console.log(calculate("1182*123"));
