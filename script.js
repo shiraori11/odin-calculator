8e59357a6708908af755be5d788eb6edd7cf99ab
shiraori11 <shiraori@proton.me>
2026-05-27 15:34:34 +0800

    Reworked calculator logic I reworked the calculator logic again. I think this one is readable than my previous iteration. While it's still a mess, It provides a much better functionality for the calculator while also making adjustment easier. I made some hacks to make this iteration work like using the calculateAnother to check if the user pressed another operater after the other instead of equal button

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const calculate = function(num1, num2, operator) {
 }
 
 const changeDisplay = function(button) {
+  
   // if (displayCalculation.textContent === "Calculator" || displayCalculation.textContent === "0") {
   //   displayCalculation.textContent = button;
   // } else if (button === "=") {
@@ -39,54 +40,102 @@ const changeDisplay = function(button) {
   // } else {
   //   displayCalculation.textContent += button;
   // }
-  currentDisplay = displayCalculation.textContent;
-
-  if (currentDisplay === "0") {
-    if (button.search(/[+-/*C0=]/) < 0) {
-      displayCalculation.textContent = button;
-    }
-  } else if (button === "=") {
-    calculatorNumTwo = displayCalculation.textContent;
-    calculatedNum = calculate(calculatorNumOne, calculatorNumTwo, calculatorOperator);
-
-    displayCalculation.textContent = calculatedNum;
-    calculatorOperator = null;
-    calculatorNumOne = calculatedNum;
-  } else if (button === "C") {
-    displayCalculation.textContent = 0;
+  // currentDisplay = displayCalculation.textContent;
+  // if (button === "C") {
+  //     displayCalculation.textContent = 0;
+  //     displayOperator.textContent = ""
+  //     calculatorNumOne = null;
+  //     calculatorNumTwo = null;
+  //     calculatorOperator = null;
+  // } else if (currentDisplay === "0") {
+  //   if (button.search(/[+-/*0=]/) < 0) {
+  //     displayCalculation.textContent = button;
+  //   }
+  // } else if (button === "=") {
+  //   calculatorNumTwo = displayCalculation.textContent;
+  //   calculatedNum = calculate(calculatorNumOne, calculatorNumTwo, calculatorOperator);
+  //
+  //   displayCalculation.textContent = calculatedNum;
+  //   calculatorOperator = null;
+  //   calculatorNumOne = calculatedNum;
+  // } else if (button.search(/[+-/*]/) > -1) {
+  //   displayOperator.textContent = button;
+  //
+  //   if (calculatorOperator != undefined) {
+  //     calculatorNumTwo = displayCalculation.textContent;
+  //
+  //     calculatedNum = calculate(calculatorNumOne, calculatorNumTwo, button);
+  //
+  //     displayCalculation.textContent = calculatedNum;
+  //     calculatorNumOne = calculatedNum;
+  //     calculatorNumTwo = "calculated";
+  //   } else {
+  //     calculatorNumOne = displayCalculation.textContent;
+  //     calculatorOperator = button;
+  //     displayCalculation.textContent = 0;
+  //   }
+  // } else {
+  //   if (calculatorOperator != undefined && calculatorNumTwo === "calculated") {
+  //     displayCalculation.textContent = button;
+  //     calculatorNumTwo = 0
+  //   } else {
+  //     displayCalculation.textContent += button;
+  //   }
+  // }
+  const calculatorNumTwo = displayCalculation.textContent;
+  
+  if (button === "C") {
     calculatorNumOne = null;
-    calculatorNumTwo = null;
     calculatorOperator = null;
-  } else if (button.search(/[+-/*]/) > -1) {
-
-    if (calculatorOperator != undefined) {
-      calculatorNumTwo = displayCalculation.textContent;
-      
-      calculatedNum = calculate(calculatorNumOne, calculatorNumTwo, calculatorOperator);
-      
-      displayCalculation.textContent = calculatedNum;
-      calculatorNumOne = calculatedNum;
-      calculatorNumTwo = "calculated";
+    updateDisplayNumber(0);
+    displayOperator.textContent = "";
+  } else if (isButtonOperator(button)) {
+    if (calculatorNumOne != undefined && calculatorOperator != undefined && calculateAnother === "no") {
+      calculatedNumber = calculate(calculatorNumOne, calculatorNumTwo, calculatorOperator);
+      calculatorNumOne = calculatedNumber;
+      calculateAnother = "yes";
+      calculatorOperator = button;
+      updateDisplayNumber(calculatedNumber);
     } else {
       calculatorNumOne = displayCalculation.textContent;
       calculatorOperator = button;
-      displayCalculation.textContent = 0;
+      updateDisplayNumber(0);
     }
+  } else if (button === "=") {
+      if (calculatorOperator == undefined) {
+        console.log(calculateAnother);
+        //donothing
+      } else {
+        calculatedNumber = calculate(calculatorNumOne, calculatorNumTwo, calculatorOperator);
+        updateDisplayNumber(calculatedNumber);
+        calculatorOperator = null;
+      }
   } else {
-    if (calculatorOperator != undefined && calculatorNumTwo === "calculated") {
+    if (displayCalculation.textContent === "0") {
       displayCalculation.textContent = button;
-      calculatorNumTwo = 0
+    } else if (displayCalculation.textContent !== "0" && calculateAnother === "yes") {
+      displayCalculation.textContent = button;
+      calculateAnother = "no";
     } else {
       displayCalculation.textContent += button;
     }
   }
 }
 
+const updateDisplayNumber = function(num) {
+  displayCalculation.textContent = num;
+}
+
+const isButtonOperator = function(button) {
+  return button.search(/[+-/*]/) > -1;
+}
+
 const displayCalculation = document.querySelector(".display");
 const calculatorButtons = document.querySelectorAll("button");
+const displayOperator = document.querySelector(".display-operator")
 let calculatorNumOne;
-let calculatorNumTwo;
 let calculatorOperator;
+let calculateAnother;
 
 calculatorButtons.forEach((button) => {
   button.addEventListener("click", () => {
